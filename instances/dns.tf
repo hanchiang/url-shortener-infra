74f8890596af07c0652d5d90afcc79c8004557e1
han chiang <yaphc@hotmail.com>
2022-07-03 19:10:04 +0800

    Remove dns.tf from git

--- instances/dns.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_route53_record" "urlshortener_api_prod" {
-  zone_id = var.route53_zone_id
-  name    = "api.urlshortener.yaphc.com"
-  type    = "A"
-  ttl     = "300"
-  records = [aws_instance.web.public_ip]
-}
