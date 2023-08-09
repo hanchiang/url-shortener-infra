c92d721cef895f1f664ca47aba183eca93248513
Yap Han Chiang <yaphc@hotmail.com>
2023-08-09 21:22:06 +0800

    Feature/upgrade ec2 t4g (#15)
    * Upgrade to postgres 15, fix permission by entering postgres directory before operations
    
    * Update docker installation
    
    * Fix firewall by allowing port 80 and 443 individually. Fix maxmind geoipupdate by specifying target directory
    
    * Use aws ec2 t4g type, ubuntu 22
    
    * Set cpu credits to standard, volume id, ec2 instance type
    
    * Fix block device name
    
    * Fix nginx ssl to use fullchain.pem instead of cert.pem
    
    * Encapsulate postgres version in postgres setup
    
    * Update grafana installation for arm64
    
    * Remove -k in curl
    
    * Add variables in CI
    
    * Cleanup

--- images/scripts/install-postgres.sh
+++ images/scripts/install-postgres.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
 POSTGRES_USER_PASSWORD=$(cat $POSTGRES_PASSWORD_PATH)
-sudo rm /tmp/postgres-user-password.txt
-
-sudo apt-get update
+sudo rm $POSTGRES_PASSWORD_PATH
 
 #### Install postgres, execute database schema script
 echo "Installing postgres"
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-sudo apt-get update
-sudo apt-get -y install postgresql-13
+wget -qO- https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo tee /etc/apt/trusted.gpg.d/pgdg.asc &>/dev/null
+sudo apt update
+sudo apt install postgresql postgresql-client -y
 
+psql --version
 sudo systemctl enable postgresql
 sudo systemctl start postgresql.service
 
+
+# Create user, DB
+dir=$(pwd)
+cd ~postgres
 sudo -u postgres createuser --superuser $USER
 sudo -u postgres psql << EOF
 ALTER USER $USER PASSWORD '$POSTGRES_USER_PASSWORD';
@@ -25,6 +28,9 @@ sudo -u postgres createdb $USER
 DB_NAME="url_shortener"
 sudo -u postgres createdb $DB_NAME
 
+# Allow remote connection
+sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" /etc/postgresql/*/main/postgresql.conf
+
 # Allow password authentication
 TAB="$(printf '\t')"
 cat << EOF | sudo tee -a /etc/postgresql/*/main/pg_hba.conf > /dev/null
@@ -32,3 +38,5 @@ local${TAB}all${TAB}all${TAB}password
 EOF
 
 sudo systemctl restart postgresql.service
+
+cd $dir
